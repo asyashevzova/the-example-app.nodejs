1740bcb1614f2a94bacf5563e9744a20b9a883ea
asyashevzova <asyashevtsova@yahoo.com>
2018-09-11 13:33:18 -0400

    fixed node installation script

--- scripts/nodeinstall.sh
+++ scripts/nodeinstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt-get update && sudo apt get install nodejs && sudo apt-get install npm
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install nodejs -y && sudo apt-get install npm -y
 echo "Nodejs and npm are installed, we can proceed!"
