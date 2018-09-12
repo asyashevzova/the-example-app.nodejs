13aca48098b05f4705f7be6bc525588cdfbd1630
asyashevzova <asyashevtsova@yahoo.com>
2018-09-12 09:58:09 -0400

    Updated node installation script

--- scripts/nodeinstall.sh
+++ scripts/nodeinstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install nodejs -y && sudo apt-get install npm -y
+sudo apt-get update && sudo apt-get upgrade -y && curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh && sudo bash nodesource_setup.sh && sudo apt-get install nodejs -y && sudo apt-get install npm -y
 echo "Nodejs and npm are installed, we can proceed!"
