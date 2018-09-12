63596f8e590e0008125d1c48cca7d2f7a1b9dd7a
asyashevzova <asyashevtsova@yahoo.com>
2018-09-12 11:10:27 -0400

    corrected startup scripts

--- scripts/appdevstart.sh
+++ scripts/appdevstart.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+cd /home/ubuntu
+npm install
 npm run start:dev
 echo "The app is started in development, check http://localhost:3000 "
--- scripts/appprodstart.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-npm run start:production
-echo "The app is started in production, please check http://localhost:3000 "
